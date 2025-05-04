39c2503e497c39a567a7fb820d1c369382700a43
Aman Rajput <rajputaman724857@gmail.com>
2025-05-04 20:09:28 +0530

    routing using params

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import { Route, Routes } from "react-router-dom";
+import ItemDetail from "./ItemDetail";
+import ItemList from "./ItemList";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        {/* <ItemList/> */}
+        <Routes>
+          <Route path="/" element={<ItemList/>}/>
+          <Route path="/items/:id" element={<ItemDetail/>}/>
+        
+        </Routes>
     </div>
   )
 }
