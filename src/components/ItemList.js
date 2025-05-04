cef29732f7a7f57310748e5a495e367c58fe3f2d
Aman Rajput <rajputaman724857@gmail.com>
2025-05-04 20:15:14 +0530

    add item list heading

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const ItemList = () => {
   return (
     <div>
+        <h1>Item List</h1>
         <ul>
             <li><Link to="/items/1">Item 1</Link></li>
             <li><Link to="/items/2">Item 2</Link></li>
